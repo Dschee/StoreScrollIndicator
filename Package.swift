9fb08cb3b116adee9d260ac938a4491600284896
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-05-14 14:24:31 +0200

    Add Carthage support

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "StoreScrollIndicator",
+    platforms: [.iOS("10.0")],
     products: [
         .library(name: "StoreScrollIndicator", targets: ["StoreScrollIndicator"])
     ],
---
+++ StoreScrollIndicator/StoreScrollIndicator-iOS/StoreScrollIndicator_iOS.h
@@ -0,0 +1,19 @@
+//
+//  StoreScrollIndicator_iOS.h
+//  StoreScrollIndicator-iOS
+//
+//  Created by Cihat Gündüz on 14.05.19.
+//  Copyright © 2019 usagimaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoreScrollIndicator_iOS.
+FOUNDATION_EXPORT double StoreScrollIndicator_iOSVersionNumber;
+
+//! Project version string for StoreScrollIndicator_iOS.
+FOUNDATION_EXPORT const unsigned char StoreScrollIndicator_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreScrollIndicator_iOS/PublicHeader.h>
+
+
