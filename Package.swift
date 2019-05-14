dcafe7a6973bcc109787ab09979ed57b6cf9bc0c
Cihat Gündüz <commits@cihatguenduez.de>
2019-05-14 13:55:32 +0200

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "StoreScrollIndicator",
+    products: [
+        .library(name: "StoreScrollIndicator", targets: ["StoreScrollIndicator"])
+    ],
+    targets: [
+        .target(
+            name: "StoreScrollIndicator",
+            path: "StoreScrollIndicator"
+        )
+    ]
+)
